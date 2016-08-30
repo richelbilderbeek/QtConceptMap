10ebd63676dae402e588749dfb54c0c917f22a21
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-30 21:16:56 +0200

    Do not ignore OCLint

--- do_oclint.sh
+++ do_oclint.sh
@@ -40,7 +40,7 @@ fail=`egrep "Compiler Errors" oclint.log | wc -l`
 if [ $fail -eq 1 ]; 
 then
   echo "OCLint: Compiler error"
-  #exit 1
+  exit 1
 else
   echo "OCLint: OK"
 fi
@@ -54,5 +54,5 @@ then
   echo "OCLint: OK"
 else
   echo "OCLint: Fail"
-  #exit 1
+  exit 1
 fi
