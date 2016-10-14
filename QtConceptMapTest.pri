1a71e8925bf74db27f1ca480bd01b96100a94e39
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-14 16:28:00 +0200

    Codecov

--- QtConceptMapTest.pri
+++ QtConceptMapTest.pri
@@ -1,26 +1,27 @@
 SOURCES += \
+    $$PWD/qtconceptmapbrushfactory_test.cpp \
     $$PWD/qtconceptmapcenternode_test.cpp \
     $$PWD/qtconceptmapcompetency_test.cpp \
+    $$PWD/qtconceptmapconceptdialog_test.cpp \
     $$PWD/qtconceptmapconcepteditdialog_test.cpp \
     $$PWD/qtconceptmapedgedialog_test.cpp \
+    $$PWD/qtconceptmapexampledialog_test.cpp \
     $$PWD/qtconceptmapexamplesdialog_test.cpp \
+    $$PWD/qtconceptmaphelper_test.cpp \
+    $$PWD/qtconceptmapitemhighlighter_test.cpp \
     $$PWD/qtconceptmapnewarrow_test.cpp \
     $$PWD/qtconceptmapnodedialog_test.cpp \
     $$PWD/qtconceptmapqtedgedialog_test.cpp \
     $$PWD/qtconceptmapqtedge_test.cpp \
     $$PWD/qtconceptmapqtnodedialog_test.cpp \
     $$PWD/qtconceptmapqtnodefactory_test.cpp \
-    $$PWD/qtconceptmaprateconcepttallydialog_test.cpp \
-    $$PWD/qtconceptmaprateexamplesdialog_test.cpp \
     $$PWD/qtconceptmapqtnode_test.cpp \
-    $$PWD/qtconceptmap_test.cpp \
-    $$PWD/qtconceptmaphelper_test.cpp \
-    $$PWD/qtconceptmapbrushfactory_test.cpp \
-    $$PWD/qtconceptmapexampledialog_test.cpp \
     $$PWD/qtconceptmaprateconceptdialog_test.cpp \
+    $$PWD/qtconceptmaprateconcepttallydialog_test.cpp \
     $$PWD/qtconceptmapratedconceptdialog_test.cpp \
+    $$PWD/qtconceptmaprateexamplesdialog_test.cpp \
     $$PWD/qtconceptmaprating_test.cpp \
-    $$PWD/qtconceptmapitemhighlighter_test.cpp
+    $$PWD/qtconceptmap_test.cpp
 
 HEADERS += \
     $$PWD/*_test.h
--- main_test.cpp
+++ main_test.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <QtTest/QtTest>
+#include "qtconceptmapbrushfactory_test.h"
 #include "qtconceptmapcenternode_test.h"
 #include "qtconceptmapcompetency_test.h"
+#include "qtconceptmapconceptdialog_test.h"
 #include "qtconceptmapconcepteditdialog_test.h"
 #include "qtconceptmapedgedialog_test.h"
+#include "qtconceptmapexampledialog_test.h"
 #include "qtconceptmapexamplesdialog_test.h"
 #include "qtconceptmaphelper_test.h"
 #include "qtconceptmapitemhighlighter_test.h"
@@ -14,44 +17,43 @@
 #include "qtconceptmapqtnodedialog_test.h"
 #include "qtconceptmapqtnodefactory_test.h"
 #include "qtconceptmapqtnode_test.h"
-#include "qtconceptmaprateconcepttallydialog_test.h"
-#include "qtconceptmaprateexamplesdialog_test.h"
-#include "qtconceptmap_test.h"
-#include "qtconceptmaprating_test.h"
-#include "qtconceptmapbrushfactory_test.h"
-#include "qtconceptmapratedconceptdialog_test.h"
 #include "qtconceptmaprateconceptdialog_test.h"
-#include "qtconceptmapexampledialog_test.h"
+#include "qtconceptmaprateconcepttallydialog_test.h"
+#include "qtconceptmapratedconceptdialog_test.h"
+#include "qtconceptmaprateexamplesdialog_test.h"
+#include "qtconceptmaprating_test.h"
+#include "qtconceptmap_test.h"
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   int error = 0;
-  //{ ribi::cmap::qtconceptmapqtnode_test t; error |= QTest::qExec(&t, argc, argv); }
-  //return 0;
+  //{ ribi::cmap::qtconceptmapconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  //return 1;
 
   //These are all tests in alphabetical order
-  { ribi::cmap::qtconceptmaprating_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapbrushfactory_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmapratedconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmaprateconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmapexampledialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapcenternode_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapcompetency_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmapconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapconcepteditdialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapedgedialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmapexampledialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapexamplesdialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmaphelper_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapitemhighlighter_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapnewarrow_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapnodedialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapqtedgedialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapqtedge_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmaphelper_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapqtnodedialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapqtnodefactory_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmaprateconcepttallydialog_test t; error |= QTest::qExec(&t, argc, argv); }
-  { ribi::cmap::qtconceptmaprateexamplesdialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmapqtnode_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmaprateconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmaprateconcepttallydialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmapratedconceptdialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmaprateexamplesdialog_test t; error |= QTest::qExec(&t, argc, argv); }
+  { ribi::cmap::qtconceptmaprating_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::cmap::qtconceptmap_test t; error |= QTest::qExec(&t, argc, argv); }
 
   if (error == 0) { std::cout << "Pass\n"; }
