eb957bf269253ff9a6069ccdbbacceffaa0e1f59
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-14 11:13:25 +0200

    Codecov

--- qtconceptmapratedconceptdialog_test.cpp
+++ qtconceptmapratedconceptdialog_test.cpp
@@ -6,7 +6,7 @@
 
 void ribi::cmap::qtconceptmapratedconceptdialog_test::show_normal()
 {
-  const auto conceptmap = ConceptMapFactory().Get2();
+  const auto conceptmap = ConceptMapFactory().Get11();
   const auto nodes = GetNodes(conceptmap);
   for (const auto node: nodes)
   {
