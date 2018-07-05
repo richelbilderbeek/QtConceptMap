9143240023b7c8b84197dee6b6b8cc2ce9266217
richelbilderbeek <richel@richelbilderbeek.nl>
2018-07-05 07:49:14 +0200

    ParseCommandCreateNewNode parses correctly

--- qtconceptmapcommandcreatenewnode_test.cpp
+++ qtconceptmapcommandcreatenewnode_test.cpp
@@ -6,7 +6,9 @@
 void ribi::cmap::QtConceptMapCommandCreateNewNodeTest::Parse() const noexcept
 {
   QtConceptMap q;
-  QVERIFY(ParseCommandCreateNewNode(q, "nonsense") == nullptr);
+  {
+    QVERIFY(ParseCommandCreateNewNode(q, "nonsense") == nullptr);
+  }
   {
     const auto c = ParseCommandCreateNewNode(
       q, "create_new_node(my text, 10, 20, normal)"
@@ -14,7 +16,6 @@ void ribi::cmap::QtConceptMapCommandCreateNewNodeTest::Parse() const noexcept
     QVERIFY(c);
     assert(c);
     q.DoCommand(c);
-    QSKIP("ParseCommandCreateNewNode must be parsed correctly (EASY)", "");
     QVERIFY( (c->GetX() - 10.0) < 1.0);
     QVERIFY( (c->GetY() - 20.0) < 1.0);
     QVERIFY(c->GetText() == "my text");
