231281306481ce5611b567d206acfae990788219
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-10 07:30:34 +0200

    Test correct suggested rating for complexity, progress https://github.com/richelbilderbeek/BrainWeaver/issues/276

--- qtconceptmaprating_test.cpp
+++ qtconceptmaprating_test.cpp
@@ -30,22 +30,29 @@ void ribi::cmap::QtConceptMapRatingTest::default_suggestions()
 {
   //Number of edges, number of examples
   QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 0) == 0);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 1) == 0);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 2) == 1);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 3) == 1);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 4) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(0, 5) == 2);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 0) == 0);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 1) == 1);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 2) == 1);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 3) == 1);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 4) == 1);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 5) == 1);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 3) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 4) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(1, 5) == 2);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 0) == 1);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 1) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 1) == 1);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 2) == 2);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 3) == 2);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 4) == 2);
   QVERIFY(RatingComplexity::SuggestComplexityDefault(2, 5) == 2);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 0) == 2);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 6) == 2);
-  QVERIFY(RatingComplexity::SuggestComplexityDefault(4, 7) == 2);
-
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 0) == 1);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 1) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 2) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 3) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 4) == 2);
+  QVERIFY(RatingComplexity::SuggestComplexityDefault(3, 5) == 2);
   //Number of examples
   QVERIFY(RatingConcreteness::SuggestConcretenessDefault(0) == 0);
   QVERIFY(RatingConcreteness::SuggestConcretenessDefault(1) == 0);
