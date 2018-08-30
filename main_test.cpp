c0c4f828dcd2decaed536a52efa9dfc6c2df2ecc
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-30 17:20:16 +0200

    QtRateExamplesDialog must have one example

--- main_test.cpp
+++ main_test.cpp
@@ -47,8 +47,8 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   int error = 0;
-  { QtRateExamplesDialogTest t; error |= QTest::qExec(&t, argc, argv); }
-  assert(!"Don't forget to let Travis test everything");
+  //{ QtRateExamplesDialogTest t; error |= QTest::qExec(&t, argc, argv); }
+  //assert(!"Don't forget to let Travis test everything");
 
   //These are all tests in alphabetical order
   { CompetencyTest t; error |= QTest::qExec(&t, argc, argv); }
