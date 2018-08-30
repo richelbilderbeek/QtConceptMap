07400418c400e2cb2cf1f19e948627e68de43ac6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-30 14:43:20 +0200

    BrainWeaver update

--- main_test.cpp
+++ main_test.cpp
@@ -47,6 +47,7 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   int error = 0;
+  //{ QtCommandDeleteSelectedTest t; error |= QTest::qExec(&t, argc, argv); }
   //assert(!"Don't forget to let Travis test everything");
 
   //These are all tests in alphabetical order
